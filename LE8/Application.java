abece80268548c40df4ecb727e2cb44ae0a3ad90
221b453 <221b453@juetguna.in>
2025-05-01 14:49:21 +0530

    Add files via upload

---
+++ LE8/Application.java
@@ -0,0 +1,12 @@
+public class Application{
+    public static void main(String [] args){
+        Crow crow = new IndianCrow();
+        CrowAdapter ca = new CrowAdapter(crow);
+        client(ca);
+    }
+    public static void client(Swan swan){
+        swan.eat();
+        swan.swim();
+        swan.sing();
+    }
+}
---
+++ LE8/IndianCrow.java
@@ -0,0 +1,11 @@
+public class IndianCrow implements Crow{
+    public void cry(){
+        System.out.println("crowing");
+    }
+    public void fly(){
+        System.out.println("flying high over the mountain");
+    }
+    public void eat(){
+        System.out.println("Eating pearls");
+    }
+}
